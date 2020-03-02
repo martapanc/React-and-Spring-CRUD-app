2f1799719cfea21326013220a3c95b3066ef524c
Marta Pancaldi <marta.panc@gmail.com>
2020-03-02 20:56:51 +0000

    Add prod environment

--- src/main/java/com/example/crudreact/config/SecurityConfiguration.java
+++ src/main/java/com/example/crudreact/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.crudreact.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -30,6 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @Profile("dev")
     public RequestCache refererRequestCache() {
         return new HttpSessionRequestCache() {
             @Override
